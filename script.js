c2ed0e09c09a7af6e5d94d73a60925b691e42efa
MattFofo <matteo.segattini2@gmail.com>
2022-03-01 15:48:33 +0100

    pulizia + debug

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 
 
-const userWord = prompt('inserisci parola');
-console.log(userWord);
+//variabili
+const userWord = document.querySelector("#input").value;
+const btnStart = document.querySelector("#btn-start");
+const output = document.querySelector("#output");
 
 
 //funzione che scompone la stringa 'userInput'(parametro funzione) in un arrey di lettere, poi inverte l'ordine delle lettere e le riunisce in una nuova stringa ottenendo la parola al contrario. 
@@ -20,18 +22,21 @@ function isPalindrome(userInput) {
     if (userInput == userWordReversed) {
         return true;
         
+    }  
+}
+
+
+
+btnStart.addEventListener('click', function() {
+    const userWord = document.querySelector("#input").value;
+
+
+    //richiamo la funzione 
+    if (isPalindrome(userWord)) {
+        output.innerHTML = 'Hai trovato una parola palindroma!';
+        
+    }else {
+        output.innerHTML = 'niente, non è un palindromo..'
     }
 
-
-
-    
-}
-
-//richiamo la funzione 
-
-if (isPalindrome(userWord)) {
-    alert('è palindroma');
-    
-}else {
-    alert('non è palindroma');
-}
+})
